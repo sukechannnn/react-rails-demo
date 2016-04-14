9f568b140dd2cb7f2b5daff0babc6da4edab618c
yosuke0315 <wonderfulworld0315@gmail.com>
2016-04-14 21:10:15 +0900

    Componentにデータ渡すところまで

--- app/assets/javascripts/components/comment_box.js.jsx
+++ app/assets/javascripts/components/comment_box.js.jsx
@@ -3,19 +3,36 @@ var CommentBox = React.createClass({
     return (
       <div className="commentBox">
         <h1>Comments</h1>
-        <CommentList />
+        <CommentList data={this.props.data} />
         <CommentForm />
       </div>
     );
   }
 });
 
+// var CommentList = React.createClass({
+//   render: function() {
+//     return (
+//       <div className="commentList">
+//         <Comment author="Ishii Yuya">- This is first comment</Comment>
+//         <Comment author="haruhik">- yoo</Comment>
+//       </div>
+//     );
+//   }
+// });
+
 var CommentList = React.createClass({
   render: function() {
+    var commentNodes = this.props.data.map(function (comment) {
+      return (
+        <Comment author={comment.author}>
+          {comment.text}
+        </Comment>
+      );
+    });
     return (
       <div className="commentList">
-        <Comment author="Ishii Yuya">- This is first comment</Comment>
-        <Comment author="haruhik">- yoo</Comment>
+        {commentNodes}
       </div>
     );
   }
